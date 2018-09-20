b3d928a94f056505039ed3ea6dbd43075de2d220
= <blaize.kaye@gmail.com>
2018-09-20 12:09:06 +1200

    Adds further socket functionality for large UDP datagrams

--- lib/LagoonLogstashPusher.php
+++ lib/LagoonLogstashPusher.php
@@ -1,2 +1,24 @@
 <?php
 
+class LagoonLogstashPusher {
+
+  public static function pushUdp($host, $port, $payload) {
+
+    $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    if (!$socket) {
+      throw new Exception('Could not open UDP socket for logstash: ' . $errstr);
+    }
+
+    try {
+      $msg = json_encode($payload) . "\n";
+      if(!socket_sendto($socket, $msg, strlen($msg), $flags = 0, $host, $port)) {
+        throw new Exception('Could not send message to Logstash server: ' . $err);
+      }
+    } catch (Exception $ex) {
+      //we'll rethrow this, but we need to run some cleanup
+      throw $ex;
+    } finally {
+      socket_close($socket);
+    }
+  }
+}
